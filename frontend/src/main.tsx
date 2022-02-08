68211029d62cb48d02dd794007dfa94928af2750
DavidShefcik <davidshefcik@gmail.com>
2022-02-07 21:09:36 -0600

    chore: frontend project layout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom";
 
-import "./index.css";
-import App from "./App";
+import App from "~/layout/App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+// @ts-ignore - No types for React 18 yet
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />);
