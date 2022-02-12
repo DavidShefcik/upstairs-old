5f110477b20ab346cbd5ef101f7ecf4ecb8e14d5
DavidShefcik <davidshefcik@gmail.com>
2022-02-12 15:46:57 -0600

    feat: frontend reset password page

--- frontend/src/layout/Navigation.tsx
+++ frontend/src/layout/Navigation.tsx
@@ -5,6 +5,7 @@ import HomePage from "~/pages/index";
 import LoginPage from "~/pages/Login";
 import RegisterPage from "~/pages/Register";
 import ForgotPasswordPage from "~/pages/ForgotPassword";
+import ResetPasswordPage from "~/pages/ResetPassword";
 import NotFound from "~/pages/NotFound";
 
 export default function Navigation() {
@@ -16,6 +17,7 @@ export default function Navigation() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
---
+++ frontend/src/pages/ResetPassword.tsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { gql } from "@apollo/client";
+import { Text, Link as ChakraLink } from "@chakra-ui/react";
+
+import BaseAuthentication, {
+  BaseAuthenticationInput,
+} from "~/layout/BaseAuthentication";
+import { INPUT_SETTINGS } from "~/constants/inputs";
+
+const RESET_PASSWORD_MUTATION = gql`
+  mutation ResetPasswordMutation($password: String!) {
+    resetPassword(password: $password) {
+      success
+    }
+  }
+`;
+
+interface ResetPasswordFields {
+  password: string;
+  confirmPassword: string;
+}
+interface ResetPasswordResponse {
+  success: boolean;
+}
+
+export default function ResetPassword() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+
+  const onSuccess = (data: ResetPasswordResponse) => {
+    const { success } = data;
+
+    console.log({ success });
+  };
+
+  return (
+    <BaseAuthentication<ResetPasswordFields, ResetPasswordResponse>
+      {...{ isSubmitting, setIsSubmitting, onSuccess }}
+      title="Reset Password"
+      data={{
+        password,
+        confirmPassword,
+      }}
+      mutation={RESET_PASSWORD_MUTATION}
+      setErrors={{
+        password: setPasswordError,
+        confirmPassword: setConfirmPasswordError,
+      }}
+      fields={{
+        password: {
+          fieldName: "Password",
+          isRequired: true,
+        },
+        confirmPassword: {
+          fieldName: "Confirm password",
+          isRequired: true,
+        },
+      }}
+    >
+      <BaseAuthenticationInput
+        type="password"
+        label="Password"
+        name="password"
+        error={passwordError}
+        disabled={isSubmitting}
+        maxLength={INPUT_SETTINGS.password.maxLength}
+        autoComplete="new-password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+        isRequired
+        autoFocus
+      />
+      <BaseAuthenticationInput
+        type="password"
+        label="Confirm Password"
+        name="confirmPassword"
+        error={confirmPasswordError}
+        disabled={isSubmitting}
+        maxLength={INPUT_SETTINGS.password.maxLength}
+        autoComplete="new-password"
+        value={confirmPassword}
+        onChange={(event) => setConfirmPassword(event.target.value)}
+        isRequired
+      />
+    </BaseAuthentication>
+  );
+}
