05d9205890e7bce1de54cecb181968d816979c53
DavidShefcik <davidshefcik@gmail.com>
2022-03-20 15:39:40 -0500

    refactor: remove unused imports in Account settings

--- frontend/src/pages/settings/Account.tsx
+++ frontend/src/pages/settings/Account.tsx
@@ -1,33 +1,9 @@
-import { useState } from "react";
-import {
-  Text,
-  Box,
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  VStack,
-} from "@chakra-ui/react";
-import { gql, useMutation } from "@apollo/client";
-
 import SettingsContent from "~/layout/pages/settings/SettingsContent";
 import UpdateEmail from "~/layout/pages/settings/account/UpdateEmail";
 import UpdateAddress from "~/layout/pages/settings/account/UpdateAddress";
 import DangerZone from "~/layout/pages/settings/account/DangerZone";
 
 export default function AccountSettings() {
-  // const [
-  //   requestAccountUpdate,
-  //   { loading: updateAccountLoading, error: updateAccountError },
-  // ] = useMutation(UPDATE_ACCOUNT_MUTATION, {
-  //   variables: {
-  //     firstName,
-  //     lastName,
-  //     streetAddress,
-  //     cityAddress,
-  //     zipCodeAddress,
-  //   },
-  // });
-
   return (
     <SettingsContent title="Account Settings">
       <UpdateEmail />
