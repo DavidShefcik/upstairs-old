0505069ba41b908565d2e64b83a2851351f0465c
DavidShefcik <davidshefcik@gmail.com>
2022-05-30 01:40:22 -0500

    refactor: remove session settings

--- frontend/src/pages/settings/Security.tsx
+++ frontend/src/pages/settings/Security.tsx
@@ -1,14 +1,12 @@
 import SettingsContent from "~/layout/pages/settings/SettingsContent";
 import ChangePassword from "~/layout/pages/settings/security/ChangePassword";
 import ManageTwoFactorAuthentication from "~/layout/pages/settings/security/ManageTwoFactorAuthentication";
-import SessionManagement from "~/layout/pages/settings/security/SessionManagement";
 
 export default function SecuritySettings() {
   return (
     <SettingsContent title="Security Settings">
       <ChangePassword />
       <ManageTwoFactorAuthentication />
-      <SessionManagement />
     </SettingsContent>
   );
 }
